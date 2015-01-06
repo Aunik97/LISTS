65e85a211b44894b7d3b8233349f5056ea68bf83
Aunik Hussain <31704@longroad.ac.uk>
2015-01-06 15:19:33 +0000

    d

---
+++ table prac.py
@@ -0,0 +1,8 @@
+
+players = [["name","Kill","Deaths"],
+          ["kil1mAchine",51,49],
+          ["bob224",5,99],
+          ["mAxOr12",72,30]]
+
+print({1})
+
---
+++ tables.py
@@ -0,0 +1,17 @@
+
+
+players = [["name","Kill","Deaths"],
+          ["kil1mAchine",51,49],
+          ["bob224",5,99],
+          ["mAxOr12",72,30]]
+
+table = (players)
+def table(table):
+    for count in range(4):
+        print("{0} {1} {2} {3}".format(players[count][0],players[count][1],players[count][2],players[count][3]))
+    return
+
+table(table)
+        
+        
+
