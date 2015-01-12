cfc3c1503f894ca4793ffb2b8390edc7432fcc30
Aunik Hussain <31704@longroad.ac.uk>
2015-01-12 12:18:55 +0000

    d

---
+++ test.py
@@ -0,0 +1,54 @@
+#Aunik Hussain
+#12/01/15
+
+
+
+
+
+
+def linear_search(search_list, search_item):
+    found = False
+    index = 0
+    while not found and index < len(people):
+        if search_list == people[index]:
+            print("Found")
+            found = True
+        else:
+            print("Not Found")
+            index = index + 1
+        return found
+
+def bubble_sort(unsoreted):
+    peoples =
+    
+        
+        
+        
+        
+        
+    
+
+
+
+
+
+    return sorted_list
+    
+
+
+
+
+
+
+#Main
+
+people = ["Aunik", "Harry", "Matt", "Ben", "Dan", "Indi", "John"]
+search_item = input("enter a name")
+search_list = search_item
+linear_search(search_list, search_item)
+
+
+for index in people:
+    print(index)
+    
+    
